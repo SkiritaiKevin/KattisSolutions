3c1dbcc0b61bb2cf68dc00469de3df73b761b321
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2024-09-27 14:31:56 -0400

    Create tetration.java

---
+++ java/tetration.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class tetration {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double N = in.nextDouble();
+        in.close();
+        System.out.println(Math.pow(N, 1/N));
+    }
+}
