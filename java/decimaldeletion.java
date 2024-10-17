7fc817594d617bede10de13e2be5c9a5edb4fdea
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2024-10-17 11:27:47 -0400

    Create decimaldeletion.java

---
+++ java/decimaldeletion.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+
+public class decimaldeletion {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in); 
+
+        float n = scnr.nextFloat(); 
+        System.out.println(Math.round(n)); 
+
+        scnr.close(); 
+    }
+}
