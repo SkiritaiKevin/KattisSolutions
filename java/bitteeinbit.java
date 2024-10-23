25405c6a7aea4c71ee847244a84b41bceebaddce
Kevin Beideman <112713469+SkiritaiKevin@users.noreply.github.com>
2024-10-23 12:03:22 -0400

    Create bitteeinbit.java

---
+++ java/bitteeinbit.java
@@ -0,0 +1,13 @@
+import java.util.*; 
+
+public class bitteeinbit{
+    public static void main(String [] args){
+        Scanner scnr = new Scanner(System.in); 
+        
+        String n = scnr.next();
+        System.out.println(n.charAt(0)); 
+        
+        
+        scnr.close();
+    }
+}
